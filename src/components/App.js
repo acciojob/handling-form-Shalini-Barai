6d05d0a665fe62036ac56fc25eafa08a9b7330f4
Shalini-Barai <115135802+Shalini-Barai@users.noreply.github.com>
2023-03-25 17:56:49 +0530

    hf

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [inp,setInp]=useState("");
+  function handleChange(e){
+        setInp(e.target.value);
+        console.log(e.target.value);
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <input type="text" value={inp} onChange={handleChange}/>
     </div>
   )
 }
